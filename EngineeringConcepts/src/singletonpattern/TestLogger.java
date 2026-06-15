38741b6cbd3fa6501a7c6fdce69951636c4aa311
Vidhya12-v <Vsvidhya8156@gmail.com>
2026-06-16 01:02:53 +0530

    Completed Exercise 1 - Singleton Pattern (Logger)

--- EngineeringConcepts/src/singletonpattern/TestLogger.java
+++ EngineeringConcepts/src/singletonpattern/TestLogger.java
@@ -1,4 +1,18 @@
 package singletonpattern;
 
 public class TestLogger {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+
+        Logger logger3 = Logger.getInstance();
+        logger3.log("Third message");
+
+        System.out.println("Are all instances same? " + (logger1 == logger2 && logger2 == logger3));
+    }
 }
